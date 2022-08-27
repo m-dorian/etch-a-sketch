abb40356944019d7768595f7aacf02cc19a567f5
macridorian <macridorian9@gmail.com>
2022-08-27 19:41:11 +0300

    Remade forming of the grid  and page arrangement

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
-let gridSize = Number(prompt('Grid size:'));
-const gridRange = document.querySelector('#range-button'); 
-gridRange.addEventListener('click', function(e){ 
-   window.location.reload();
-
-}) 
-
-gridSize *= gridSize;
-let squareSize = Math.sqrt((500**2)/(gridSize));
+let gridSize; 
+let squareSize;
+const gridSizeInput = document.querySelector('#grid-size'); 
 const drawingArea = document.querySelector('.drawing-area'); 
 
-    for(let i = 0; i < gridSize; i++){ 
-        square = document.createElement('div');
+
+gridSizeInput.addEventListener('change', function(e) { 
+    while (drawingArea.hasChildNodes()) {
+        drawingArea.removeChild(drawingArea.firstChild);
+      } ;
+    ;
+    gridSize = e.target.valueAsNumber; 
+ squareSize = Math.sqrt((500**2)/(gridSize**2));
+
+
+    for(let i = 0; i < gridSize**2; i++){ 
+        let square = document.createElement('div');
         square.classList.add('grid');  
         square.style.height = `${squareSize}px`;
-        square.style.width = `${squareSize}px`;
+        square.style.width = `${squareSize}px`; 
         drawingArea.appendChild(square); 
-        square = 'undefined';
+        square = '';
 } 
+changeSquareColor();
+})
+
+
+
 
 const colorPicker = document.querySelector('#color-select');
 let colorValue;
 colorPicker.addEventListener('change', function(e) { 
     colorValue = e.target.value; 
+    return colorValue;
     
-})
-
+}) 
+  
+function changeSquareColor(){
 const gridSquares = document.querySelectorAll('.grid'); 
-
 gridSquares.forEach(grid => {  
     grid.addEventListener('mouseover', () => { 
         grid.style.backgroundColor = `${colorValue}`;
     })
     
 })
+}
